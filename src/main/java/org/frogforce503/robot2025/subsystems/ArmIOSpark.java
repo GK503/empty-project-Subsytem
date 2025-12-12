706ac3fe56db3e5500b280dafe310eed069bcd34
Gowtham Kodali <Gowtham.Kodali@frogforce503.org>
2025-12-11 19:29:09 -0500

    Finished - Checkpoint_2

--- src/main/java/org/frogforce503/robot2025/subsystems/ArmIOSpark.java
+++ src/main/java/org/frogforce503/robot2025/subsystems/ArmIOSpark.java
@@ -9,93 +9,94 @@ import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
 public class ArmIOSpark implements ArmIO{
-    SparkAbsoluteEncoder encoder;
-    SparkMax motor;
-    SparkMaxConfig motorconfig;
+   SparkAbsoluteEncoder encoder;
+   SparkMax motor;
+   SparkMaxConfig motorconfig;
 
-    public ArmIOSpark(int CANID, MotorType MotorType){
-        motor = new SparkMax(CANID, MotorType);
-        encoder = motor.getAbsoluteEncoder();
-        motorconfig = new SparkMaxConfig();
-        motorconfig.closedLoop.pid(0.0, 0.0, 0.0);
-        motorconfig.idleMode(IdleMode.kBrake);
-        motorconfig.absoluteEncoder.zeroOffset(0.0);
-        motor.configure(motorconfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    }
+   public ArmIOSpark(int CANID, MotorType MotorType){
+       motor = new SparkMax(CANID, MotorType);
+       encoder = motor.getAbsoluteEncoder();
+       motorconfig = new SparkMaxConfig();
+       motorconfig.closedLoop.pid(0.0, 0.0, 0.0);
+       motorconfig.idleMode(IdleMode.kBrake);
+       motorconfig.absoluteEncoder.zeroOffset(0.0);
+       motor.configure(motorconfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+   }
 
-    @Override
-    public void updateInputs(ArmIOInputs inputs){
-    inputs.data = new ArmIOData(
-        encoder.getPosition(),
-        encoder.getVelocity(),
-        false,
-        motor.getMotorTemperature(),
-        motor.getBusVoltage(),
-        motor.getOutputCurrent()
-      );
-    }
+   @Override
+   public void updateInputs(ArmIOInputs inputs){
+   inputs.data = new ArmIOData(
+       getPosition(),
+       getVelocity(),
+       isMotorConnected(),
+       getMotorTemperature(),
+       getBusVoltage(),
+       getOutputCurrent()
+     );
+   }
 
-    @Override
-    public double getPosition(){
-        return encoder.getPosition();
-    }
+   @Override
+   public double getPosition(){
+       return encoder.getPosition();
+   }
 
-    @Override
-    public double getVelocity(){
-        return encoder.getVelocity();
-    }
+   @Override
+   public double getVelocity(){
+       return encoder.getVelocity();
+   }
 
-    @Override
-    public void setPosition(double setPoint){
-        motor.getClosedLoopController().setReference(setPoint, ControlType.kVelocity);
-    }
+   @Override
+   public void setPosition(double setPoint){
+       motor.getClosedLoopController().setReference(setPoint, ControlType.kVelocity);
+   }
 
-    @Override
-    public void setPID(double p, double i, double d) {
-        motorconfig.closedLoop.p(p);
-        motorconfig.closedLoop.i(i);
-        motorconfig.closedLoop.d(d);
-        motor.configure(motorconfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
-    }
+   @Override
+   public void setPID(double p, double i, double d) {
+       motorconfig.closedLoop.p(p);
+       motorconfig.closedLoop.i(i);
+       motorconfig.closedLoop.d(d);
+       motor.configure(motorconfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+   }
 
-    @Override
-    public void setIdleMode(boolean isBrake){
-        if (isBrake == true){
-            motorconfig.idleMode(IdleMode.kBrake);
-        } else {
-            motorconfig.idleMode(IdleMode.kCoast);
-        }
-        motor.configure(motorconfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
-    }
+   @Override
+   public void setIdleMode(boolean isBrake){
+       if (isBrake == true){
+           motorconfig.idleMode(IdleMode.kBrake);
+       } else {
+           motorconfig.idleMode(IdleMode.kCoast);
+       }
+       motor.configure(motorconfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+   }
 
-    @Override
-    public void stop(){
-       motor.stopMotor();
-    }
+   @Override
+   public void stop(){
+      motor.stopMotor();
+   }
 
-    @Override
-    public void reset(){
-        motor.getClosedLoopController().setReference(0.0, ControlType.kVelocity);
-    }
+   @Override
+   public void reset(){
+       motor.getClosedLoopController().setReference(0.0, ControlType.kVelocity);
+   }
 
-    @Override
-    public double getTemp(){
-        return motor.getMotorTemperature();
-    }
+   @Override
+   public double getTemp(){
+       return motor.getMotorTemperature();
+   }
 
-    @Override
-    public double getBusVoltage(){
-        return motor.getBusVoltage();
-    }
+   @Override
+   public double getBusVoltage(){
+       return motor.getBusVoltage();
+   }
 
-    @Override
-    public double getOutputCurrent(){
-        return motor.getOutputCurrent();
-    }
+   @Override
+   public double getOutputCurrent(){
+       return motor.getOutputCurrent();
+   }
 
-    @Override
-    public boolean isMotorConnected(){
-        double voltage = motor.getBusVoltage();
-        return voltage > 1.0;
-    }
+   @Override
+   public boolean isMotorConnected(){
+       double voltage = motor.getBusVoltage();
+       return voltage > 0.0;
+   }
 }
+
